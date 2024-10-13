6453a56de9b023a14cc375767f632635560a2859
LounesMD <lounes.meddahi@gmail.com>
2024-10-13 19:17:38 +0200

    Add 383

---
+++ problems/algorithms/383_Ranson_Note.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        letters = dict()
+
+        for letter in magazine:
+            if letter in letters:
+                letters[letter] += 1 
+            else:
+                letters[letter] = 1 
+
+        for letter in ransomNote:
+            if letter not in letters:
+                return False
+
+            else:
+                if letters[letter] == 0:
+                    return False
+                else:
+                    letters[letter] -= 1
+
+        return True 
