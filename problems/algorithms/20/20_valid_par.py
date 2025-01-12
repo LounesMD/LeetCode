a6f45b76e6f739dfd7f8b146b18f51c710f7b62a
LounesMD <lounes.meddahi@gmail.com>
2025-01-12 09:14:51 +0100

    Add 20

---
+++ problems/algorithms/20/20_valid_par.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:    
+        res = []
+        for elt in s:
+            if elt in "{[(":
+                res.append(elt)
+            else:
+                if (not res) or \
+                    (elt == ")" and res[-1] != "(") or \
+                    (elt == "}" and res[-1] != "{") or \
+                    (elt == "]" and res[-1] != "["):
+                    return False
+                res.pop()
+        return True 
