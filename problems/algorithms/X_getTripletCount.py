3ccc9e9d8db73372ac476eb1ad4638ac2b7d822f
LounesMD <lounes.meddahi@gmail.com>
2025-03-13 16:40:07 +0100

    Add X

--- problems/algorithms/X_getTripletCount.py
+++ problems/algorithms/X_getTripletCount.py
@@ -1,14 +1,19 @@
-def getTripletCount1(a, d):
+def getTripletIndices(a, d):
     """
-    Complexity: O(n^3).
-    Better solution with one or two iterations using dict.
+    d = a_i + a_j + a_k
     """
-    cpt = 0
-    for i in range(len(a) - 2):
-        for j in range(i+1, len(a) - 1):
-            for k in range(j+1, len(a)):
-                if ((a[i] + a[j] + a[k]) % d == 0):
-                    cpt += 1
-    return cpt
+    n = len(a)
+    result = []  # Stores index triplets
+    for i in range(n):
+        new_target = d - a[i]
+        res = {}  # Dictionary to store {value: index}
+        for j in range(i + 1, n):  # Ensure distinct indices
+            if a[j] in res:  # Found a valid triplet
+                result.append((i, res[a[j]], j))  # Store indices
+            
+            res[new_target - a[j]] = j  # Store {needed complement: index} for future lookups
+    
+    return result
 
-print(getTripletCount(a=[3, 3,4, 7, 8], d = 5))
+
+print(getTripletIndices(a=[3, 3,4, 2, 1, 2, 7, 8], d = 5))
